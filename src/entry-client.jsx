97d27766b9b424c77532d54d8df68eb31e0fcaa1
Karim Piyar Ali <karimpa@fb.com>
2025-04-29 22:55:54 -0700

    readme + error display

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -5,3 +5,10 @@ hydrateRoot(
   document.getElementById('root'),
   <App suspenseId={document.getElementById("suspenseId").dataset.suspenseId}/>,
 );
+
+window.onerror = function onerror(event, source, lineno, colno, error) {
+  let pre = document.createElement('pre');
+  pre.style.color = 'red';
+  pre.innerHTML = `============\nON ERROR\n${error.message}\n${error.stack}`;
+  document.body.appendChild(pre);
+}
